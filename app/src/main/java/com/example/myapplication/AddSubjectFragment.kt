8fda6d476b7af6a70ece725c42cca06d6610205c
Sorawit <sorawit32@gmail.com>
2019-09-27 00:15:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/AddSubjectFragment.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AddSubjectFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AddSubjectFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.add_subject_fragment, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.toolbar.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val frameId = R.id.fragment_container
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.tool_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.addSubject -> {
+                replaceFragment(AddSubjectFragment.newInstance())
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun AppCompatActivity.replaceFragment(fragment: Fragment) {
+        supportFragmentManager.inTransaction {
+            setCustomAnimations(R.anim.fragment_in, R.anim.fragment_out).replace(frameId, fragment)
+        }
+    }
+
+    private inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+        beginTransaction().func().commit()
+    }
+
+}
